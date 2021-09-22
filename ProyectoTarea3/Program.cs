a4f5032e13f9d588f4b9ed97b4a37a1dff2fe477
Alvaro Starling Hernandez Luzon <alvarostarling24@gmail.com>
2021-09-21 20:26:48 -0400

    Tarea 3

---
+++ ProyectoTarea3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProyectoTarea3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Capitulo6 c = new Capitulo6();
+            Console.WriteLine("---Ejercicios 1, 2 y 3---");
+            Capitulo6.Ejercicio3();
+            Console.Write("\n\n\n Precione ENTER para continuar con el ejercicio 5...");
+            Console.ReadLine();
+            Console.WriteLine("\n\n\n ---Ejercicio 5---");
+            c.Ejercicio5();*/
+            Capitulo7.Ejercicio5();
+        }
+    }
+}
